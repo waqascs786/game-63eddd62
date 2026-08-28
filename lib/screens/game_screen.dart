1f5bfffba959ac1472b913a5dad89037cea76d79
waqascs786 <waqas.cs786@gmail.com>
2026-08-28 23:07:15 +0500

    Initial game source code

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -12,39 +12,49 @@ class GameScreen extends StatefulWidget {
 
 class _GameScreenState extends State<GameScreen> {
   int _currentLevel = 0;
-  int _coins = 100;
   final IAPService _iap = IAPService();
+
+  int get _coins => _iap.coins;
 
   @override
   void initState() {
     super.initState();
+    _iap.onPurchased = () { if (mounted) setState(() {}); };
+    _iap.initialize(gameId: widget.config.id);
     if (widget.config.iapEnabled) {
-      _iap.initialize();
       _iap.loadProducts();
     }
   }
 
   @override
   void dispose() {
-    if (widget.config.iapEnabled) _iap.dispose();
+    _iap.dispose();
     super.dispose();
   }
 
   void _onLevelComplete(int coinsEarned) {
     setState(() {
-      if (widget.config.iapEnabled) _coins += coinsEarned;
+      if (widget.config.iapEnabled) _iap.addCoins(coinsEarned);
       _currentLevel++;
     });
   }
 
   void _onHintUsed() {
-    if (widget.config.iapEnabled && _coins >= 20) {
-      setState(() => _coins -= 20);
+    if (widget.config.iapEnabled) {
+      setState(() => _iap.spendCoins(5));
     }
   }
 
   void _onSpendCoins(int amount) {
-    setState(() { _coins -= amount; });
+    setState(() {
+      if (amount > 0) { _iap.spendCoins(amount); }
+      else { _iap.addCoins(-amount); }
+    });
+  }
+
+  void _onBuyCoins(String productId) {
+    final product = _iap.products.firstWhere((p) => p.id == productId, orElse: () => throw Exception("Product not found: $productId"));
+    _iap.buyCoins(product);
   }
 
   void _onPrevLevel() {
@@ -70,6 +80,7 @@ class _GameScreenState extends State<GameScreen> {
         onLevelComplete: _onLevelComplete,
         onHintUsed: _onHintUsed,
         onSpendCoins: _onSpendCoins,
+        onBuyCoins: widget.config.iapEnabled ? _onBuyCoins : null,
         onNavigatePrev: _currentLevel > 0 ? _onPrevLevel : null,
         onNavigateNext: _currentLevel < widget.config.levels.length - 1 ? _onNextLevel : null,
       ),
