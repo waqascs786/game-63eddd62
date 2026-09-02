c2b1e5334b1b16bd693ee005424f2cd4b52e196f
waqascs786 <waqas.cs786@gmail.com>
2026-09-02 16:58:36 +0500

    Initial game source code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/splash_screen.dart';
@@ -6,20 +6,35 @@ import 'theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: 'AIzaSyAzN4vJZlYmGVzm8sCsz1bEPawQEiKIc6k',
-      appId: defaultTargetPlatform == TargetPlatform.iOS
-          ? '1:311998863107:ios:placeholder'
-          : defaultTargetPlatform == TargetPlatform.android
-              ? '1:311998863107:android:placeholder'
-              : '1:311998863107:web:76d4b682624e96d02cfb36',
-      messagingSenderId: '311998863107',
-      projectId: 'trivianinja-bff5c',
-      storageBucket: 'trivianinja-bff5c.firebasestorage.app',
-      iosBundleId: defaultTargetPlatform == TargetPlatform.iOS ? 'com.trivianinja.app' : null,
-    ),
-  );
+  try {
+    final options = kIsWeb
+        ? const FirebaseOptions(
+            apiKey: 'AIzaSyAzN4vJZlYmGVzm8sCsz1bEPawQEiKIc6k',
+            appId: '1:311998863107:web:76d4b682624e96d02cfb36',
+            messagingSenderId: '311998863107',
+            projectId: 'trivianinja-bff5c',
+            storageBucket: 'trivianinja-bff5c.firebasestorage.app',
+          )
+        : defaultTargetPlatform == TargetPlatform.iOS
+            ? const FirebaseOptions(
+                apiKey: 'AIzaSyAzN4vJZlYmGVzm8sCsz1bEPawQEiKIc6k',
+                appId: '1:311998863107:ios:placeholder',
+                messagingSenderId: '311998863107',
+                projectId: 'trivianinja-bff5c',
+                storageBucket: 'trivianinja-bff5c.firebasestorage.app',
+                iosBundleId: 'com.game15.ios',
+              )
+            : const FirebaseOptions(
+                apiKey: 'AIzaSyAzN4vJZlYmGVzm8sCsz1bEPawQEiKIc6k',
+                appId: '1:311998863107:android:placeholder',
+                messagingSenderId: '311998863107',
+                projectId: 'trivianinja-bff5c',
+                storageBucket: 'trivianinja-bff5c.firebasestorage.app',
+              );
+    await Firebase.initializeApp(options: options);
+  } catch (e) {
+    debugPrint('Firebase init failed: ' + e.toString());
+  }
   runApp(const TriviaGameApp());
 }
 
